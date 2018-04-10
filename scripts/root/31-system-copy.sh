fe4b884ab5d8b541c1ac694fa1baeb54370216a2
icmx <icmx@protonmail.com>
2018-04-10 15:28:27 +1000

    root scripts fixed

---
+++ scripts/root/31-system-copy.sh
@@ -0,0 +1,13 @@
+set -e
+echo "<root> Copying system configuration ..."
+
+cp --force --recursive "${O_TARGETS_ROOT_PATH}/etc" "/"
+cp --force --recursive "${O_TARGETS_ROOT_PATH}/bin" "/usr"
+
+ln --force --relative --symbolic                        \
+  "/usr/share/fontconfig/conf.avail/70-no-bitmaps.conf" \
+  "/etc/fonts/conf.d"
+
+ln --force --relative --symbolic --no-target-directory \
+"/etc/issue.${O_PALTFORM}"                             \
+"/etc/issue"
