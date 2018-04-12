8f6c0caaa92907ddf0c5f0721d887517afeae92d
icmx <icmx@protonmail.com>
2018-04-12 23:48:07 +1000

    scripts/root: variables export fix

--- scripts/root/32-system-fill.sh
+++ scripts/root/32-system-fill.sh
@@ -1,5 +1,5 @@
 set -e
 echo "<root> Filling system configuration ..."
 
-echo "ODG_PLATFORM=\"${O_PLATFORM}\"" >> "/etc/profile.d/16-custom-variables.sh"
+echo "export ODG_PLATFORM=\"${O_PLATFORM}\"" >> "/etc/profile.d/16-custom-variables.sh"
 echo -e "127.0.0.1\tic.o" >> "/etc/hosts"
