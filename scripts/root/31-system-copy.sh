d60bf9e3b877b815b07a45dc77a46148461a4b02
icmx <icmx@protonmail.com>
2018-04-22 16:03:39 +1000

    obin: special directory for custom executables

--- scripts/root/31-system-copy.sh
+++ scripts/root/31-system-copy.sh
@@ -4,11 +4,18 @@ echo "<root> Copying system configuration ..."
 cp --force --recursive "$O_RESOURCES/$USER/etc" "/"
 cp --force --recursive "$O_RESOURCES/$USER/bin" "/usr"
 
+
 ln --force --relative --symbolic                        \
   "/usr/share/fontconfig/conf.avail/70-no-bitmaps.conf" \
   "/etc/fonts/conf.d"
 
 ln --force --relative --symbolic --no-target-directory \
-"/etc/issue.${O_PLATFORM}"                             \
-"/etc/issue"
+  "/etc/issue.$O_PLATFORM"                             \
+  "/etc/issue"
 
+
+mkdir --parents "/usr/obin"
+
+for file in $O_RESOURCES/root/bin/*; do
+  ln --force --relative --symbolic "/usr/bin/$file" "/usr/obin"
+done
