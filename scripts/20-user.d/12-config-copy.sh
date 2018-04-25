8fede4f6aeb2f07a3dfa26b5df2d9112b1dd49f0
icmx <icmx@protonmail.com>
2018-04-25 22:56:46 +1000

    minor update

--- scripts/20-user.d/12-config-copy.sh
+++ scripts/20-user.d/12-config-copy.sh
@@ -4,4 +4,4 @@ echo "<user> Copying user-specific configuration ..."
 cp --recursive --force --no-target-directory "${O_RESOURCES}/${USER}/local/var"   "${HOME}/.local/var"
 cp --recursive --force --no-target-directory "${O_RESOURCES}/${USER}/local/share" "${HOME}/.local/share"
 
-ln --relative --symbolic --no-target-directory "${HOME}/.local/bin" "${O_RESOURCES}/${USER}/local/bin"
+ln --relative --symbolic --no-target-directory "${O_RESOURCES}/${USER}/local/bin" "${HOME}/.local/bin"
