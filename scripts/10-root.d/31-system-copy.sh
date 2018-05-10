c48e6d44c9eaaa91596043ea17a61e0df38f1913
icmx <icmx@protonmail.com>
2018-05-10 23:11:12 +1000

    rxvt: vtwheel extesion moved to root

--- scripts/10-root.d/31-system-copy.sh
+++ scripts/10-root.d/31-system-copy.sh
@@ -1,30 +1,23 @@
 set -e
 echo "<root> Copying system configuration ..."
 
-cp --force --recursive "$O_RESOURCES/$USER/etc" "/"
-cp --force --recursive "$O_RESOURCES/$USER/bin" "/usr"
+# /resources/root e.g.
+PREF="$O_RESOURCES/$USER"
+
+# Executables
+cp --force --recursive "$PREF/bin" "/usr/local"
+cp --force --recursive "$PREF/lib" "/usr/local"
+
+ln --force --relative --symbolic --no-target-directory "/usr/bin/startx" "/usr/local/bin/x"
+ln --force --relative --symbolic --no-target-directory "/usr/bin/ranger" "/usr/local/bin/e"
+ln --force --relative --symbolic --no-target-directory "/usr/bin/bash"   "/usr/local/bin/b"
+
+# Making patched version of startx - will be patched in the next script
+cp --force "/usr/bin/startx" "/usr/local/bin/startx"
 
 
-ln --force --relative --symbolic                        \
-  "/usr/share/fontconfig/conf.avail/70-no-bitmaps.conf" \
-  "/etc/fonts/conf.d"
+# Configuration
+cp --force --recursive "$PREF/etc" "/"
 
-ln --force --relative --symbolic --no-target-directory \
-  "/etc/issue.$O_PLATFORM"                             \
-  "/etc/issue"
-
-ln --force --relative --symbolic --no-target-directory \
-  "/usr/bin/startx"  "/usr/local/bin/x"
-
-ln --force --relative --symbolic --no-target-directory \
-  "/usr/bin/ranger"  "/usr/local/bin/e"
-
-ln --force --relative --symbolic --no-target-directory \
-  "/usr/bin/bash"  "/usr/local/bin/b"
-
-mkdir --parents "/usr/obin"
-
-for file in $O_RESOURCES/root/bin/*
-do
-  ln --force --relative --symbolic "/usr/bin/$file" "/usr/obin"
-done
+ln --force --relative --symbolic                       "/usr/share/fontconfig/conf.avail/70-no-bitmaps.conf" "/etc/fonts/conf.d"
+ln --force --relative --symbolic --no-target-directory "/etc/issue.$O_PLATFORM"                      "/etc/issue"
--- scripts/10-root.d/33-system-patch.sh
+++ scripts/10-root.d/33-system-patch.sh
@@ -3,7 +3,7 @@ echo "<root> Patching additional system components ..."
 
 # /bin/startx patch - it's acrually a shell script
 # After patch it will read ~/.config/xorg/xinitrc istead of ~/.xinitrc
-sed --in-place "s:\$HOME/.xinitrc:\$\{HOME\}/.config/xorg/xinitrc:g" "/bin/startx"
+sed --in-place "s:\$HOME/.xinitrc:\$HOME/.config/xorg/xinitrc:g" "/usr/local/bin/startx"
 
 # /etc/sudoers pacth
 # Removes "$USER" template by actual user name
