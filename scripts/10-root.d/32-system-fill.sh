f374c5fb62bb23ac7739f07ac9a94eb68874f772
icmx <icmx@protonmail.com>
2018-05-21 22:39:00 +1000

    sudoers: update

--- scripts/10-root.d/32-system-fill.sh
+++ scripts/10-root.d/32-system-fill.sh
@@ -3,3 +3,5 @@ echo "<root> Filling system configuration ..."
 
 echo "export ODG_PLATFORM=\"${O_PLATFORM}\"" >> "/etc/profile.d/16-custom-variables.sh"
 echo -e "127.0.0.1\tic.o" >> "/etc/hosts"
+
+usermod --append --groups "odg" "${O_NONROOT}"
--- scripts/10-root.d/33-system-patch.sh
+++ scripts/10-root.d/33-system-patch.sh
@@ -4,7 +4,3 @@ echo "<root> Patching additional system components ..."
 # /bin/startx patch - it's acrually a shell script
 # After patch it will read ~/.config/xorg/xinitrc istead of ~/.xinitrc
 sed --in-place "s:\$HOME/.xinitrc:\$HOME/.config/xorg/xinitrc:g" "/usr/local/bin/startx"
-
-# /etc/sudoers pacth
-# Removes "$USER" template by actual user name
-sed --in-place "s:\$USER:${O_NONROOT}:g" "/etc/sudoers"
