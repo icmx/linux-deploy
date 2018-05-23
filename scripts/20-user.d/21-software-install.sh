3992b25392ddd4339cc746222a06f8959539fcea
icmx <icmx@protonmail.com>
2018-05-23 16:02:34 +1000

    scripts: user apps installer update

--- scripts/20-user.d/21-software-install.sh
+++ scripts/20-user.d/21-software-install.sh
@@ -1,32 +1,52 @@
 set -e
 echo "<user> Installing user-specific software ..."
 
+
 pi() {
-  # Pseudo-installation. Parameters:
+  # pi - pseudo-installation
   #
-  #   - $1  a file name (with no extension)
-  #   - $2  a file extension
-  #   - $3  URL to file download
-  #
+  #   name  file name without extension
+  #   ext   file extension (.tar.gz e.g.)
+  #   link  url to file
 
-  local PREF="$HOME/.local"
-  local NAME="$PREF/$1.$2"
+  local name="${1}"
+  local  ext="${2}"
+  local link="${3}"
 
-  echo "   Downloading $3 ..."
-  curl --progress-bar --location --url "$3" --output "$NAME"
-  mkdir --parents "$PREF/opt/$1"
-  tar   --extract --verbose --file "$NAME" --directory "$PREF/opt/$1" --strip-components 1
-  rm    "$NAME"
+  local prefix="${HOME}/.local"
+  local file="${prefix}/${name}.${ext}"
+
+  echo "    Downloading ${link} to ${file} ..."
+  curl --progress-bar --location --url "${link}" --output "${file}"
+  mkdir --parents "${prefix}/opt/${name}"
+  tar --extract --verbose --file "${file}" --directory "$PREF/opt/${name}" --strip-components 1
+  rm "${file}"
 }
 
-tor_version=$(
-  curl --silent "https://www.torproject.org/projects/torbrowser.html" | \
-  grep --perl-regexp --only-matching "<th>GNU/Linux.+\(\K[^\)]+"
-)
+
+lv() {
+  # lv - latest version
+  #
+  #    link        url to page where lv should find the varsion number
+  #    expression  regular expression to retrieve version number
+  #
+  # Dedicated to those nice people who can't make an always-latest
+  # symlink at their repos. Thaks, I really appreciate that.
+
+  local link="${1}"
+  local expression="${2}"
+
+  curl --silent --location                         -- "${link}"       |\
+  grep --perl-regexp --only-matching --max-count 1 -- "${expression}"
+}
+
+tor_version=$(lv "http://torproject.org/projects/torbrowser.html" "<th>GNU/Linux.+\(\K[^\)]+")
+sublimetext_version=$(lv "http://sublimetext.com/3" "Build \K\d+")
+
 
 pi "firefox-stable" "tar.bz2" "http://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
 pi "firefox-beta"   "tar.bz2" "http://download.mozilla.org/?product=firefox-beta-latest&os=linux64&lang=en-US"
-pi "sublime_text"   "tar.bz2" "http://download.sublimetext.com/sublime_text_3_build_3176_x64.tar.bz2"
+pi "sublime_text"   "tar.bz2" "http://download.sublimetext.com/sublime_text_3_build_${sublimetext_version}_x64.tar.bz2"
 pi "tor-browser"    "tar.xz"  "http://www.torproject.org/dist/torbrowser/${tor_version}/tor-browser-linux64-${tor_version}_en-US.tar.xz"
 
 cp --recursive "$HOME/.local/var/mozilla/firefox/ProfilesTemplate" "$HOME/.local/var/mozilla/firefox/Profiles/Beta"
